58cd698899bc5d0b74f37bbc5302d72201dfc95c
Jessica Turner <jmcclung3509@gmail.com>
2023-10-03 08:33:06 -0700

    New text

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<main class="space-y-36 lg:space-y-52 relative">
 		<section class="pt-36 xl-pt-40 fold animate hero container mx-auto">
-			<h1 class="text-4xl text-purple-dark">  SOme new text to see if git works</h1>
+			<h1 class="text-4xl text-purple-dark">  SOme new and now works</h1>
 		</section>
 	</main>
 </template>
